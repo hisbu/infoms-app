0c8ce1abd9a217077f48bc11f3e4e9cb74ef7384
hisbu <43975159+hisbu@users.noreply.github.com>
2020-07-12 11:19:58 +0700

    first commit

---
+++ precache-manifest.fcee0543ecdc63289e8ef62acb72b24f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d8ab24b05a4fb2c95cf6d2cbac7417",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29eb34762746d2b32155",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "afff218ac995aad3f915",
+    "url": "/static/css/main.0ed407ad.chunk.css"
+  },
+  {
+    "revision": "29eb34762746d2b32155",
+    "url": "/static/js/2.dbd04989.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.dbd04989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096de556a86e710153cb",
+    "url": "/static/js/3.5ecb05fe.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/static/js/3.5ecb05fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afff218ac995aad3f915",
+    "url": "/static/js/main.e023fafb.chunk.js"
+  },
+  {
+    "revision": "1d456c4a25666cacb187",
+    "url": "/static/js/runtime-main.455da302.js"
+  },
+  {
+    "revision": "a01f2118b046844f2c08b3e9da32d7be",
+    "url": "/static/media/261000.a01f2118.jpg"
+  },
+  {
+    "revision": "8504a8e9636594872be2b7e4ddc9676a",
+    "url": "/static/media/Comfortaa-Bold.8504a8e9.ttf"
+  },
+  {
+    "revision": "f40889caa25956ef4d27c2aadc4fbe13",
+    "url": "/static/media/Comfortaa-VariableFont_wght.f40889ca.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/static/media/Ubuntu-Regular.2505bfbd.ttf"
+  },
+  {
+    "revision": "8d634f4318355157a82864437e5e90ee",
+    "url": "/static/media/Vollkorn-VariableFont_wght.8d634f43.ttf"
+  },
+  {
+    "revision": "6bbb21afa6b2e996352d8cec88bacfb4",
+    "url": "/static/media/alarm2.6bbb21af.mp3"
+  },
+  {
+    "revision": "1ee65867c9ccb1f09fed1f471abc4705",
+    "url": "/static/media/ashar.1ee65867.png"
+  },
+  {
+    "revision": "ab3686c9de7420888ca5ee1bcc70e665",
+    "url": "/static/media/dzuhur.ab3686c9.png"
+  },
+  {
+    "revision": "2ca712a51c50b0e26328e658f646d98e",
+    "url": "/static/media/isya.2ca712a5.png"
+  },
+  {
+    "revision": "c02c507c97d236559e78588b383ebb0f",
+    "url": "/static/media/logo.c02c507c.jpg"
+  },
+  {
+    "revision": "7f57e05ae38eb661d5d3a5ff806e6fa6",
+    "url": "/static/media/magrib.7f57e05a.png"
+  },
+  {
+    "revision": "3c9156d924b033c59112de2a95b11655",
+    "url": "/static/media/subuh.3c9156d9.png"
+  },
+  {
+    "revision": "9884f356c0ae81fdb7dd000be6f5d871",
+    "url": "/static/media/syuruq.9884f356.png"
+  }
+]);
